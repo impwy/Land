31bc53aa0d21eb6c4a169ac45a36c51eb2f30cc5
sabsiru <zero013@naver.com>
2023-03-17 10:16:42 +0900

    no message

--- src/main/java/com/land/myapp/model/board/BoardService.java
+++ src/main/java/com/land/myapp/model/board/BoardService.java
@@ -1,5 +1,21 @@
 package com.land.myapp.model.board;
 
+import java.util.List;
+
 public interface BoardService {
 
+    //글 등록
+    void insertBoard(BoardVO vo);
+
+    //글 수정
+    void updateBoard(BoardVO vo);
+
+    //글 삭제
+    void deleteBoard(BoardVO vo);
+
+    //글 상세
+    BoardVO getBoard(BoardVO vo);
+
+    //글 목록
+    List<BoardVO> getBoardList(BoardVO vo);
 }
