79c6398722daa29d18727bfa592b301836f3a57e
yoo geon hee <jerry57454@naver.com>
2023-03-20 09:22:10 +0900

    no message

---
+++ src/main/java/com/land/myapp/member/membercontroller.java
@@ -0,0 +1,19 @@
+package com.land.myapp.member;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class membercontroller {
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login() {
+		return "member/login";
+	}
+
+	@RequestMapping(value = "/time", method = RequestMethod.GET)
+	public String time() {
+		return "menu/time";
+	}
+}
