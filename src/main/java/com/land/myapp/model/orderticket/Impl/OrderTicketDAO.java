f61be9cb360461a8de93e88b820e4a30c2fc0b98
zuhwan <wnghks4386@gmail.com>
2023-03-21 15:45:29 +0900

    no message

--- src/main/java/com/land/myapp/model/orderticket/Impl/OrderTicketDAO.java
+++ src/main/java/com/land/myapp/model/orderticket/Impl/OrderTicketDAO.java
@@ -1,5 +1,9 @@
 package com.land.myapp.model.orderticket.Impl;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -22,4 +26,10 @@ public class OrderTicketDAO {
 		mybatis.delete("OrderTicketDAO.deleteOrderTicket",vo);
 	}
 	//티켓 조회
+	public List<OrderTicketVO> selectOrderTicket(OrderTicketVO vo){
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("member_id", vo.getMember_id());
+		map.put("OrderTicket", vo);
+		return mybatis.selectList("OrderTicketDAO.ordeticketMember" , map);
+	}
 }
