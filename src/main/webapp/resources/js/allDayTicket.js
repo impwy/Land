af8d4d29eb32564926eb09fab39b797e485dd5a2
yong <pwy6817@gmail.com>
2023-03-22 14:31:43 +0900

    no message

--- src/main/webapp/resources/js/allDayTicket.js
+++ src/main/webapp/resources/js/allDayTicket.js
@@ -1,26 +1,4 @@
-//+,-를 누르면 getTicketPrice(TicketVO vo)를 실행해서 
-//ticket_type에 대한 가격을 가져와 금액에 추가해준다.
-//예매 페이지에 들어올 때 티켓 가격을 가져온다.
 
-
-//$('document').ready(function(){
-//	var amount;
-//	var adult;
-//	var teen;
-//	var baby;
-//	
-//var total = parsInt($('#total').val());
-////총량
-//adult =parseInt($('#adult-amount').val());
-//teen =parseInt($('#teen-amount').val());
-//baby =parseInt($('#baby-amount').val());
-//amount = "어른X"+adult+","+"청소년X"+teen+","+"아기X"+baby;
-//total = adult * 40000 + teen * 30000 + baby * 20000;
-//
-//$('#total').val(total);
-//$('#amount').val(amount);
-//
-//});
 $(document).ready(function() {
 	  // Get references to the input elements
 	  var adultInput = $("#adult-amount");
@@ -40,7 +18,9 @@ $(document).ready(function() {
 		var teenValue = parseInt(teenInput.val());
 		var babyValue = parseInt(babyInput.val());
 	    var totalValue = adultValue*40000 + teenValue*30000 + babyValue*20000;
+	    var amountValue = "어른"+adultValue+",청소년"+teenValue+",유아"+babyValue;
 	    totalInput.val(totalValue);
+	    amountInput.val(amountValue);
 	  }
 	  
 	});
