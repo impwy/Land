a230e927ce949695ef7e200bf6f991786ed77f3c
yong <pwy6817@gmail.com>
2023-03-23 16:50:45 +0900

    no message

--- src/main/java/com/land/myapp/view/BasketController.java
+++ src/main/java/com/land/myapp/view/BasketController.java
@@ -1,9 +1,6 @@
 package com.land.myapp.view;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.land.myapp.model.basket.BasketService;
 
