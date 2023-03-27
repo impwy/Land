c9c3933f2a4ae96932dfce0ae265d4d14be1fcdf
yoo geon hee <jerry57454@naver.com>
2023-03-27 14:47:30 +0900

    no message

--- src/main/java/com/land/myapp/view/basket/BasketController.java
+++ src/main/java/com/land/myapp/view/basket/BasketController.java
@@ -1,10 +1,16 @@
 package com.land.myapp.view.basket;
 
+import java.util.HashMap;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -16,36 +22,35 @@ public class BasketController {
 
 	@Autowired
 	private BasketService basketservice;
-	
+
 	@RequestMapping("/insertBasket")
 	public String insertBasket(BasketVO vo) {
 		System.out.println("ajax성공");
-		basketservice.insertBasket(vo);;
+		basketservice.insertBasket(vo);
 		return "main";
 	}
-	
 
-	/*@RequestMapping(value = "/aaa", method = RequestMethod.GET)
-	public String insertbasket(BasketVO vo) {
-		basketservice.insertBasket(vo);
-
-		return "aaa";
-
-	}
+	/*
+	 * @RequestMapping(value = "/aaa", method = RequestMethod.GET) public String
+	 * insertbasket(BasketVO vo) { basketservice.insertBasket(vo);
+	 * 
+	 * return "aaa";
+	 * 
+	 * }
+	 */
 
 	@RequestMapping(value = "/aaa", method = RequestMethod.GET)
 	public String deletebasket(BasketVO vo) {
 		basketservice.deleteBasket(vo);
 
 		return "aaa";
-	}*/
+	}
 
-	
 	@RequestMapping(value = "/basket/basketlist", method = RequestMethod.GET)
-	 public String listbasket(Model model,BasketVO vo) {
-		model.addAttribute("basketList",basketservice.getBasketList(vo));
+	public String listbasket(Model model, BasketVO vo) {
+		model.addAttribute("basketList", basketservice.getBasketList(vo));
 		System.out.print("데이터 전송 됨");
-	        return "basket/basketlist";  
-		}
+		return "basket/basketlist";
+	}
 
 }
