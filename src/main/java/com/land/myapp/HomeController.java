eb5152fda6bc1d81699c3a9a0ff4bb4e5f8a470c
yong <pwy6817@gmail.com>
2023-03-31 09:19:45 +0900

    no message

--- src/main/java/com/land/myapp/HomeController.java
+++ src/main/java/com/land/myapp/HomeController.java
@@ -11,9 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
+
 @Controller
 public class HomeController {
 
@@ -32,12 +30,13 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 
 		model.addAttribute("serverTime", formattedDate);
-
+		
 		return "main";
 
 	}
 	@RequestMapping(value = "/myapp/login", method = RequestMethod.GET)
 	public String login() {
+		
 		return "/member/login";
 
 	}
